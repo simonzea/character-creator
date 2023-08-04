060b7e3d07feabd3f1a213c577a6f837bc2bb073
Simon Zea <szeag@unal.edu.co>
2023-08-04 11:01:56 -0500

    Character-creat

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -78,6 +78,7 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
+      <div className={styles.perspective}></div>
       <div className={styles.characterWrapper}>
         <Character
           body={body}
